003ae8e21cdc00523644b081fa378d98a1d323fc
rdiehlmartinez <rdm@stanford.edu>
2019-06-01 19:14:51 -0700

    all code baby

---
+++ src/params.py
@@ -0,0 +1,26 @@
+__author__ = 'Richard Diehl Martinez'
+
+'''
+Provides a number of useful utility functions and classes for data processing,
+model intialization and model training procedures.
+'''
+
+import json
+
+################# Parameter Processing #################
+
+class Params(object):
+    '''Establishes wrapper class around parameter json file.'''
+    def __init__(self, data):
+        self.__dict__ = json.load(data)
+
+    def __repr__(self):
+        return json.dumps(self.__dict__, sort_keys=True, indent=3)
+
+def read_params(path):
+    '''
+    Reads in json parameter file and created a Params Object.
+    '''
+    assert('.json' in path), "file passed into read_params() is not in json format"
+    with open(path) as json_file:
+        return Params(json_file)
