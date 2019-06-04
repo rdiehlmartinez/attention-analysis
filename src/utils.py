440d7f9b850d9264fb47045eebf7055deddfc0f9
rdiehlmartinez <rdm@stanford.edu>
2019-06-04 11:25:34 -0700

    updated utils and models

--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,7 @@ from tqdm import tqdm_notebook as tqdm
 from sklearn.metrics import roc_auc_score
 import nltk
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import SGDClassifier
 
 ################### Data Analysis Utils ###################
 
@@ -44,13 +45,25 @@ def get_statistics(data_list, key):
 
 # For bias classification task
 def sentence_to_POS_matrix(input_toks, bias_label, indices):
-    ''' Given sentences transforms these into POS tags and then into a dict vectorizer'''
+    '''
+    Given sentences transforms these into POS tags and then into a dict vectorizer.
+    The input tokens are passed in as a dictionary where the key represents a
+    particular index in the original dataset and the corresponding value is the
+    tokenized sentence.
+    '''
+    # Typically we want these features to be added in at the start
+    # of the dataset
     labels = []
-    for i, idx in enumerate(indices): # a list of indices
-        sentence_toks = input_toks[idx] # Indexing into corresponding row entry
+    for i, idx in enumerate(indices): # a list of indices in the dataset
+
+        idx = idx.item()
+        assert(idx in input_toks.keys()), \
+            "there is an index key in the dataset for which no matching sentence exists"
+
+        sentence_toks = input_toks[idx]
         bias_index = bias_label[i] # the index of the biased word in the sentence
         labels.append({nltk.pos_tag(sentence_toks)[bias_index][1] : 1})
-    return DictVectorizer().fit_transform(labels)
+    return DictVectorizer().fit_transform(labels).toarray()
 
 ################### Prepare Model Utils ###################
 
@@ -60,26 +73,28 @@ Common inference and training functions for target task models.
 
 def logreg_train_for_epoch(self, dataloader, input_key, label_key, **kwargs):
     accuracies = []
-    for step, batch in enumerate(tqdm(dataloader)):
+    label_classes = np.unique(dataloader.dataset.data[label_key])
+    for step, batch in enumerate(dataloader):
         inputs = batch[input_key]
         labels = batch[label_key]
-        self.model = self.model.partial_fit(inputs, labels)
-        accuracies.append(self.model.score(inputs,labels))
+        self.model = self.model.partial_fit(inputs, labels, classes=label_classes)
+        accuracies.append({"num_examples":len(labels),
+                           "accuracy":self.model.score(inputs, labels)})
     return accuracies
 
 def logreg_binary_inference_func(self, dataloader, input_key, label_key, threshold=0.42, **kwargs):
     ''' For logistic regression binary-class inference'''
     predictions = []
     evaluations = []
-    for step, batch in enumerate(tqdm(dataloader)):
+    for step, batch in enumerate(dataloader):
         inputs = batch[input_key]
         labels = batch[label_key]
 
-        predict_probs = self.model.predict_proba(inputs, labels)
+        predict_probs = self.model.predict_proba(inputs)
 
         batch_predictions = []
         for prob in list(predict_probs):
-            if prob > threshold:
+            if prob[1] > threshold:
                 predictions.append(1)
                 batch_predictions.append(1)
             else:
@@ -87,9 +102,9 @@ def logreg_binary_inference_func(self, dataloader, input_key, label_key, thresho
                 batch_predictions.append(0)
 
         accuracy = np.sum(np.array(batch_predictions) == np.array(labels))/len(labels)
-        auc_score = roc_auc_score(labels, predict_probs)
+        auc_score = roc_auc_score(labels, predict_probs[:,1])
 
-        curr_eval = {"batch_size":len(labels),
+        curr_eval = {"num_examples":len(labels),
                      "accuracy":accuracy,
                      "auc":auc_score}
         evaluations.append(curr_eval)
@@ -100,15 +115,61 @@ def logreg_multi_inference_func(self, dataloader, input_key, label_key, **kwargs
     ''' For logistic regression multi-class inference'''
     predictions = []
     evaluations = []
-    for step, batch in enumerate(tqdm(dataloader)):
+    for step, batch in enumerate(dataloader):
         inputs = batch[input_key]
         labels = batch[label_key]
         predictions.append(self.model.predict_proba(inputs, labels))
         accuracy = self.model.score(inputs, labels)
-        curr_eval = {"batch_size":len(labels), "accuracy":accuracy}
+        curr_eval = {"num_examples":len(labels),
+                     "accuracy":accuracy}
         evaluations.append(curr_eval)
     predictions = np.stack(predictions, axis=0)
     return predictions, evaluations
+
+
+################### Classification Utils ###################
+
+def run_boostrapping(classification_experiment,
+                     dataset,
+                     params,
+                     input_key='input',
+                     label_key='label',
+                     threshold=0.42,
+                     statistics=["auc", "accuracy"],
+                     num_bootstrap_iters=100):
+    '''
+    Randomly shuffles dataset and reports confidence interval statistics
+    via using bootstrapping methods.
+    '''
+    stats_list = {statistic: [] for statistic in statistics}
+    for _ in tqdm(range(num_bootstrap_iters), desc='Bootstrapping'):
+        dataset.shuffle_data()
+        data_split = params.final_task['data_split']
+        batch_size = params.final_task['training_params']['batch_size']
+        loaders = dataset.split_train_eval_test(**data_split, batch_size=batch_size)
+        train_dataloader, eval_dataloader, _ = loaders
+        if params.final_task["model"] == "log_reg":
+            classification_experiment.model = SGDClassifier(loss='log')
+        else:
+            classification_experiment.reinitialize_weights()
+
+        _, evaluations = classification_experiment.train_model(train_dataloader,
+                                                                    eval_dataloader,
+                                                                    input_key=input_key,
+                                                                    label_key=label_key,
+                                                                    threshold=threshold)
+        avg_evaluations = [average_data(epoch_evaluations) for epoch_evaluations in evaluations]
+        for statistic in statistics:
+            _, max_statistic, _ = get_statistics(avg_evaluations, statistic)
+            stats_list[statistic].append(max_statistic)
+
+    return_stats = {}
+    for stat, values in stats_list.items():
+        lower_ci_bound = np.percentile(values, 2.5)
+        upper_ci_bound = np.percentile(values, 97.5)
+        avg_val = np.mean(values)
+        return_stats[stat] = [(lower_ci_bound, upper_ci_bound), avg_val]
+    return return_stats
 
 
 ################### Attention Extraction Utils ###################
