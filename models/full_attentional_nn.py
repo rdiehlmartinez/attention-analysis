c45f8e50f1373adeadee927c9588ab250b75eefc
rdiehlmartinez <rdm@stanford.edu>
2019-06-04 21:35:16 -0700

    richard baseline experiments done

--- models/full_attentional_nn.py
+++ models/full_attentional_nn.py
@@ -8,15 +8,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class FullAttentionalClassifier(nn.Module):
-    def __init__(self, num_attention_dists, attention_dim, hidden_dim, output_dim=1):
+    def __init__(self, num_attention_dists, input_dim, hidden_dim, output_dim=1):
         '''
         output dim is set to 1 for binary cross entropy loss.
         '''
         super().__init__()
+        self.weighted_combination = nn.Linear(num_attention_dists, 1)
         self.f1 = nn.Linear(input_dim, hidden_dim)
-        self.f2 = nn.Linear(hidden_dim, output_dim)
+        self.f2 = nn.Linear(hidden_dim, int(hidden_dim/2))
+        self.f3 = nn.Linear(int(hidden_dim/2), output_dim)
 
     def forward(self, X):
-        hidden = F.relu(self.f1(X))
-        output = self.f2(hidden)
+        transpose_input = torch.transpose(X, -2, -1)
+        weighted_combination = self.weighted_combination(transpose_input)
+        weighted_combination = torch.transpose(weighted_combination, -2, -1)
+        hidden = F.relu(self.f1(weighted_combination))
+        hidden = F.relu(self.f2(hidden))
+        output = self.f3(hidden)
         return output
