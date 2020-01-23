97f70bef4d65140feb4ba28330c51fec1a6c1efa
rdiehlmartinez <rdm@stanford.edu>
2020-01-23 10:06:15 -0800

    removed old bias_classification folder

--- experiments/bias_classification/__init__.py
+++
@@ -1,7 +0,0 @@
-'''
-Basic imports that allow easier file navigation. The appending to sys.path
-allows the model to be able to access other models and lib folders from the
-root.
-'''
-
-import sys; sys.path.append('../../..')
