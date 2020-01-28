eb0e90a29dd241851dfdcfdeca8fd40af793ceba
rdiehlmartinez <rdm@stanford.edu>
2020-01-27 17:29:37 -0800

    cleaned up class

--- src/params.py
+++ src/params.py
@@ -17,6 +17,7 @@ class Params(object):
     def __repr__(self):
         return json.dumps(self.__dict__, sort_keys=True, indent=3)
 
+@classmethod
 def read_params(path):
     '''
     Reads in json parameter file and created a Params Object.
@@ -24,13 +25,3 @@ def read_params(path):
     assert('.json' in path), "file passed into read_params() is not in json format"
     with open(path) as json_file:
         return Params(json_file)
-
-def intialize_params(experiment_path):
-    '''
-    Creates a param object that stores the parameters of the given current
-    experiment, and creates an Experiment object. The Experiment Object is an
-    abstract class that stores the intermediary model from which we can extract
-    attention distributions.
-    '''
-    params = read_params(experiment_path)
-    return params
