e2133345b1ca01a4a96db917125fd739e9bec048
rdiehlmartinez <rdm@stanford.edu>
2020-03-02 14:29:07 -0800

    added pos weak labeling functions

--- src/utils/weak_labeling_utils.py
+++ src/utils/weak_labeling_utils.py
@@ -136,3 +136,86 @@ def get_glove_features(dataset_params, dataset):
     # num_entries, dim
     tensor_embeddings = torch.stack(embeddings)
     return tensor_embeddings
+
+
+def get_pos_features(dataset_params, dataset):
+    '''
+    Iterates over a dataset and for the predicted biased word generates the pos
+    tag for that word.
+
+    Args:
+        * dataset_params (dict): A dictionary of parameter values specifying
+            configurations for the dataset.
+        * dataset (Experimentdataset): defined in src/dataset
+            - We require each entry in the dataset to contain information/key
+                values for the 1) pre_ids, 2) rel_ids, 3) pos_ids
+
+    Returns:
+        * tensor_features (tensor): a tensor of the same number of dimensions as the other tensors
+            in the dataset - features contains the extracted features.
+    '''
+    from .data_utils import get_id2tok
+    import nltk
+    from sklearn.feature_extraction import DictVectorizer
+
+    pos_tags = []
+    id2tok = get_id2tok(dataset_params)
+
+    for entry in dataset:
+        pre_ids = entry["pre_ids"].tolist() # the ids of the tokens prior to debias edits
+        pad_idx = pre_ids.index(0) # specify that 0 is padding id
+        toks = [id2tok[x] for x in pre_ids[:pad_idx]]
+        words, tok_to_word_idx = get_words_and_indices(toks)
+
+        # Figuring out what the index is of the first biased word
+        bias_token_idx = entry["pre_tok_label_ids"].to(dtype=torch.int).flatten().tolist().index(1)
+        bias_word_idx = tok_to_word_idx[bias_token_idx]
+
+        # looking up tag of the biased word and creating the POS matrix
+
+        pos_tag = nltk.pos_tag(words)[bias_word_idx][1]
+        pos_tags.append({pos_tag: 1})
+
+
+    pos_matrix = DictVectorizer().fit_transform(pos_tags).toarray()
+    # num_entries, num_pos
+    pos_matrix = torch.tensor(pos_matrix)
+    
+    return pos_matrix
+
+
+def get_bert_features(dataset_params, dataset):
+    '''
+    Iterates over a dataset and for the predicted biased word generates the
+    contextualized BERT embedding for that word.
+
+    Args:
+        * dataset_params (dict): A dictionary of parameter values specifying
+            configurations for the dataset.
+        * dataset (Experimentdataset): defined in src/dataset
+            - We require each entry in the dataset to contain information/key
+                values for the 1) pre_ids, 2) rel_ids, 3) pos_ids
+
+    Returns:
+        * tensor_features (tensor): a tensor of the same number of dimensions as the other tensors
+            in the dataset - features contains the extracted features.
+    '''
+    from pytorch_pretrained_bert import BertModel
+
+    model = BertModel.from_pretrained('bert-base-uncased')
+
+    embeddings = []
+
+    for entry in dataset:
+        # Figuring out what the index is of the first biased word
+        bias_token_idx = entry["pre_tok_label_ids"].to(dtype=torch.int).flatten().tolist().index(1)
+
+        # the pre_ids were tokenized with the standard BERT Tokenizer
+        outputs = model(entry["pre_ids"])[0]
+        #TODO: check how we index into the bias_token_idx for the word
+
+        embeddings.append(embedding)
+
+    # num_entries, dim
+    tensor_embeddings = torch.stack(embeddings)
+    return tensor_embeddings
