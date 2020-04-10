Hook for GRU, GRU masking bug

src/models/gru_cls.py
@@ -10,6 +10,29 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
+class AttentionLayer(nn.Module):
+    def __init__(self, units, input_dim, hidden_dim, directions):
+        super().__init__()
+        
+        # attentional layers
+        self.W1 = nn.Linear(directions * hidden_dim, units)
+        self.W2 = nn.Linear(input_dim, units)
+        self.V = nn.Linear(units, 1)
+        
+    def forward(self, enc_output, dec_hidden, attention_mask=None):
+        # score: [B, N, 1]
+        scores = self.V(torch.tanh(self.W1(dec_hidden.unsqueeze(1)) + self.W2(enc_output)))
+
+        if attention_mask is not None:
+            if len(attention_mask.shape) == 2:
+                attention_mask = attention_mask.unsqueeze(dim=2)
+            scores += attention_mask * -10000.0
+
+        # attn_weights: [B, N, 1]
+        attn_weights = torch.softmax(scores, dim=1)
+
+        return attn_weights
+
 class GRUClassifier(nn.Module):
     def __init__(self, final_task_params, attention_params=None, output_dim=1, bidirectional=True):
         '''
@@ -57,42 +80,47 @@ class GRUClassifier(nn.Module):
         # attentional layers
         context_dim = 0
         if self.attentional:
-            units = final_task_params.get("attention_units", 512)
-            self.W1 = nn.Linear(self.n_directions * self.hidden_dim, units)
-            self.W2 = nn.Linear(self.input_dim, units)
-            self.V = nn.Linear(units, 1)
+            self.attention_layer = AttentionLayer(
+                final_task_params.get("attention_units", 512),
+                self.input_dim,
+                self.hidden_dim,
+                self.n_directions
+            )
             context_dim = self.input_dim
 
         self.fc = nn.Linear(context_dim + self.n_directions * self.hidden_dim, self.output_dim)
         self.relu = nn.ReLU()
 
-    def attention(self, enc_output, dec_hidden, attention_mask):
-        # score: [B, N, 1]
-        scores = self.V(torch.tanh(self.W1(dec_hidden.unsqueeze(1)) + self.W2(enc_output)))
-        scores[attention_mask] = -10000.0
-
-        # attn_weights: [B, N, 1]
-        attn_weights = torch.softmax(scores, dim=1)
-
-        # context: [B, E]
-        context = torch.sum(attn_weights * enc_output, dim=1)
-
-        return context, attn_weights
-
-    def forward(self, x, lengths, attention_mask=None, **kwargs):
+    def forward(self, x, lengths=None, attention_mask=None, **kwargs):
 
         # packed_enc_output: [B, ?, E]
-        packed_enc_output = pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
+        if lengths is not None:
+            packed_enc_output = pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
+        else:
+            packed_enc_output = x
 
         # dec_hidden: [B, 2 * D]
         dec_output, dec_hidden = self.gru(packed_enc_output)
         dec_hidden = torch.cat([dec_hidden[-2], dec_hidden[-1]], dim=-1)
 
         # padded_enc_output: [B, N, E]
-        padded_enc_output, _ = pad_packed_sequence(packed_enc_output, batch_first=True, padding_value=0., total_length=x.shape[1])
+        if lengths is not None:
+            padded_enc_output, _ = pad_packed_sequence(
+                packed_enc_output, 
+                batch_first=True, 
+                padding_value=0.,
+                total_length=x.shape[1]
+            )
+        else:
+            padded_enc_output = x
 
         if self.attentional:
-            context, attn_weights = self.attention(padded_enc_output, dec_hidden, attention_mask.long())
+            
+            if attention_mask is not None:
+                attention_mask = attention_mask.to(dtype=torch.float)
+                
+            attn_weights = self.attention_layer(padded_enc_output, dec_hidden, attention_mask)
+            context = torch.sum(attn_weights * padded_enc_output, dim=1)
             output = self.fc(torch.cat([context, dec_hidden], dim=-1))
         else:
             output = self.fc(dec_hidden)